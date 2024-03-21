001bd07cf27cbfd57f2fe81ece5b202e24f89cdf
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-03-21 16:47:16 +0200

    add IDispenserProvider interface

--- contracts/DispenserState.sol
+++ contracts/DispenserState.sol
@@ -3,13 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@poolzfinance/poolz-helper-v2/contracts/interfaces/ILockDealNFT.sol";
 import "@poolzfinance/poolz-helper-v2/contracts/interfaces/IVaultManager.sol";
-import "@poolzfinance/poolz-helper-v2/contracts/interfaces/ISimpleProvider.sol";
 
 contract DispenserState {
     mapping(uint256 => mapping(address => bool)) public isTaken;
-
-    struct Builder {
-        ISimpleProvider simpleProvider;
-        uint256[] params;
-    }
 }
