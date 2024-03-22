12bca5437c7d4dc871d6d95edc9af2d2c828d6b4
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-03-22 12:43:32 +0200

    dispenser refactor

--- contracts/DispenserProvider.sol
+++ contracts/DispenserProvider.sol
@@ -1,14 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/interfaces/IERC20.sol";
-import "@poolzfinance/lockdeal-nft/contracts/SimpleProviders/DealProvider/DealProvider.sol";
-import "./DispenserState.sol";
+import "./DispenserInternal.sol";
 
-contract DispenserProvider is DealProvider, DispenserState {
-    using ECDSA for bytes32;
-
+contract DispenserProvider is DispenserInternal {
     constructor(ILockDealNFT _lockDealNFT) DealProvider(_lockDealNFT) {
         name = "DispenserProvider";
     }
@@ -44,48 +39,5 @@ contract DispenserProvider is DealProvider, DispenserState {
         );
         _createSimpleNFTs(poolId, owner, data);
         isTaken[poolId][owner] = true;
-    }
-
-    function _encodeBuilder(
-        Builder[] calldata builder
-    ) internal pure returns (bytes memory data) {
-        for (uint256 i = 0; i < builder.length; ++i) {
-            data = abi.encodePacked(data, address(builder[i].simpleProvider), builder[i].params);
-        }
-    }
-
-    function _createSimpleNFTs(
-        uint256 tokenPoolId,
-        address owner,
-        Builder[] calldata data
-    ) internal {
-        for (uint256 i = 0; i < data.length; ++i) {
-            uint256 poolId = lockDealNFT.mintForProvider(owner, data[i].simpleProvider);
-            data[i].simpleProvider.registerPool(poolId, data[i].params);
-            lockDealNFT.cloneVaultId(poolId, tokenPoolId);
-            poolIdToAmount[tokenPoolId] -= data[i].params[0];
-            _withdrawIfAvailable(data[i].simpleProvider, poolId, owner);
-        }
-    }
-
-    function _withdrawIfAvailable(
-        ISimpleProvider provider,
-        uint256 poolId,
-        address owner
-    ) internal {
-        if (provider.getWithdrawableAmount(poolId) > 0) {
-            lockDealNFT.safeTransferFrom(owner, address(lockDealNFT), poolId);
-        }
-    }
-
-    function _checkData(
-        uint256 poolId,
-        bytes memory data,
-        bytes memory signature
-    ) internal view returns (bool success) {
-        address signer = lockDealNFT.getData(poolId).owner;
-        bytes32 hash = keccak256(data).toEthSignedMessageHash();
-        address recoveredSigner = hash.recover(signature);
-        success = recoveredSigner == signer;
     }
 }
