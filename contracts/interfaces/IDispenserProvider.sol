d54117d1e53545c2911357e3915d197e905c5837
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-11-06 10:45:04 +0200

    add custom errors

--- contracts/interfaces/IDispenserProvider.sol
+++ contracts/interfaces/IDispenserProvider.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.0;
 import "@poolzfinance/poolz-helper-v2/contracts/interfaces/ISimpleProvider.sol";
 
 interface IDispenserProvider is ISimpleProvider {
-    event TokensDispensed(uint256 poolId, address user, uint256 amountTaken, uint256 leftAmount);
-
     struct Builder {
         ISimpleProvider simpleProvider;
         uint256[] params;
@@ -18,4 +16,13 @@ interface IDispenserProvider is ISimpleProvider {
         Builder[] calldata data,
         bytes calldata signature
     ) external;
+
+    event TokensDispensed(uint256 poolId, address user, uint256 amountTaken, uint256 leftAmount);
+
+    error CallerNotApproved(address caller, address owner, uint256 poolId);
+    error InvalidTime(uint256 currentTime, uint256 validUntil);
+    error InvalidSignature(uint256 poolId, address owner);
+    error TokensAlreadyTaken(uint256 poolId, address owner);
+    error AmountMustBeGreaterThanZero();
+    error NotEnoughTokensInPool(uint256 requestedAmount, uint256 availableAmount);
 }
