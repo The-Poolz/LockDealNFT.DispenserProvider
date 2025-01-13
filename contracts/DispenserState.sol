b1375b54ee4580e23849dcdf8bda800cf0a9c569
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-01-13 16:46:33 +0200

    use typehash as constants

--- contracts/DispenserState.sol
+++ contracts/DispenserState.sol
@@ -13,11 +13,13 @@ abstract contract DispenserState is IDispenserProvider, DealProvider {
     /// @return bool The claim status of the user in the pool.
     mapping(uint256 => mapping(address => bool)) public isTaken;
 
-    bytes32 MESSAGE_TYPEHASH =
-        keccak256(
-            "MessageStruct(Builder[] data,uint256 poolId,address receiver,uint256 validUntil)Builder(address simpleProvider,uint256[] params)"
-        );
+    /// @notice Hash of the message type for the EIP712 signature. Represented as:
+    // keccak256(
+    //     "MessageStruct(Builder[] data,uint256 poolId,address receiver,uint256 validUntil)Builder(address simpleProvider,uint256[] params)"
+    // )
+    bytes32 public constant MESSAGE_TYPEHASH = 0x1ed893638dd3a77fd536c1b602c7e1d3111ac7199bbbae36b1047344a7f518be;
 
-    bytes32 BUILDER_TYPEHASH =
-        keccak256("Builder(address simpleProvider,uint256[] params)");
+    /// @notice Hash of the builder type for the EIP712 signature. Represented as:
+    /// keccak256("Builder(address simpleProvider,uint256[] params)");
+    bytes32 public constant BUILDER_TYPEHASH = 0xa5a2654c59a9d1110e55d822043f207b74f770a35ca104ce94fedba9a3dc5173;
 }
